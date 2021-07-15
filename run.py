ceee47e7744fd89a7208cc658707331073ff87b1
easechen <956902354@qq.com>
2021-07-15 14:47:47 +0800

    增加云函数调用入口

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
 # -*- coding:UTF-8 -*-
 import sign
 
+# 云函数调用接口
+def yunRun(event, context):
+    try:
+        sign.sign()
+    except Exception as e:
+        print(e)
+        return("失败！")
+    else:
+        return("成功！")
+
+
 if __name__=='__main__':
     sign.sign()
